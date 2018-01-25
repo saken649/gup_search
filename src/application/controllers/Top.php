75811ea3b383c1c183a8ea7c5fc73e08a421769d
saken649 <saken649@KensMBP-Late2017.local>
2018-01-26 00:31:27 +0900

    first commit index.php無しのURLで/top/viewが見れるところまで

---
+++ src/application/controllers/Top.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: saken649
+ * Date: 2018/01/25
+ * Time: 23:05
+ */
+
+class Top extends CI_Controller
+{
+	public function view()
+	{
+		$this->load->view('top/top.php');
+	}
+}
---
+++ src/application/views/top/top.php
@@ -0,0 +1,9 @@
+<html lang="ja">
+	<head>
+		<title>ガルパンサーチ</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<p>ぱんつのあほー？！</p>
+	</body>
+</html>
